ce052a2ddbbef774e09d054a54d17c1faa70ab0e
Jason Chin <jason.rob.chin@gmail.com>
2019-02-11 17:01:12 -0500

    initial commit v1

---
+++ public/script.js
@@ -0,0 +1,142 @@
+var canvas, context;
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging) {
+  console.log('click', x, y, dragging)
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+function redraw(){
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+  
+  context.strokeStyle = "#000";
+  context.lineJoin = "round";
+  context.lineWidth = 10;
+			
+  for(var i=0; i < clickX.length; i++) {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+    }else{
+      context.moveTo(clickX[i]-1, clickY[i]);
+    }
+    context.lineTo(clickX[i], clickY[i]);
+    context.closePath();
+    context.stroke();
+  }
+
+  showInputImage(drawSmallImage());
+}
+
+function drawSmallImage() {
+
+  // Resize image first
+  const resizeCanvas = document.createElement('canvas');
+  resizeCanvas.width = 28;
+  resizeCanvas.height = 28;
+
+  const resizeCtx = resizeCanvas.getContext('2d');
+
+  resizeCtx.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  resizeCtx.fillStyle = "#000";
+  resizeCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+  resizeCtx.strokeStyle = "#fff";
+  resizeCtx.lineJoin = "round";
+  resizeCtx.lineWidth = 1;
+			
+  for(var i=0; i < clickX.length; i++) {		
+    resizeCtx.beginPath();
+    if(clickDrag[i] && i){
+      resizeCtx.moveTo(clickX[i-1]/10, clickY[i-1]/10);
+    }else{
+      resizeCtx.moveTo((clickX[i]-1)/10, clickY[i]/10);
+    }
+    resizeCtx.lineTo(clickX[i]/10, clickY[i]/10);
+    resizeCtx.closePath();
+    resizeCtx.stroke();
+  }
+
+  let imageDataURL = resizeCtx.canvas.toDataURL();
+
+  return imageDataURL;
+}
+
+function showInputImage(dataURL) {
+  let inputImg = document.getElementById('display-img');
+  inputImg.src = dataURL;
+
+  document.getElementById('prediction-text').hidden = true;
+}
+
+function showPrediction(conf, cls) {
+  document.getElementById('prediction-text').hidden = false;
+  let predictionText = document.getElementById('prediction-text');
+  predictionText.innerHTML = `I'm ${conf}% sure it's an <b>${cls}</b>.`;
+}
+
+window.onload = () => {
+  context = document.getElementById('draw-space').getContext("2d");
+
+  canvas = document.getElementById('draw-space');
+  canvas.addEventListener('mousedown', (e) => {
+    paint = true;
+    addClick(e.offsetX, e.offsetY);
+    redraw();
+  });
+
+  canvas.addEventListener('mousemove', (e) => {
+    if(paint){
+      addClick(e.offsetX, e.offsetY, true);
+      redraw();
+    }
+  });
+
+  canvas.addEventListener('mouseup', (e) => {
+    paint = false;
+  });
+
+  canvas.addEventListener('mouseleave', (e) => {
+    paint = false;
+  });
+
+  let clearButton = document.getElementById('clear-button');
+  clearButton.addEventListener('click', () => {
+    clickX = [];
+    clickY = [];
+    clickDrag = [];
+    redraw();
+  });
+
+  let submitButton = document.getElementById('submit-button');
+  submitButton.addEventListener('click', async (e) => {
+    submitButton.setAttribute('disabled', true);
+
+    let imageDataURL = drawSmallImage();
+
+    let pred = await fetch("http://127.0.0.1:5000/predict", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"image": imageDataURL})
+    });
+    pred = await pred.json();
+
+    showInputImage(imageDataURL);
+
+    showPrediction(pred.conf, pred.class);
+
+    setTimeout(() => {
+      submitButton.removeAttribute('disabled');
+    }, 1000)
+  });
+
+  showInputImage(drawSmallImage());
+}
---
+++ server.py
@@ -0,0 +1,71 @@
+import re
+import base64
+from PIL import Image
+import io
+import numpy as np
+
+from flask import Flask, request, send_from_directory, send_file, json
+app = Flask(__name__)
+
+from tensorflow import keras
+keras.backend.clear_session()
+
+model = None
+
+def load_model():
+    # load the pre-trained Keras model (here we are using a model
+    # pre-trained on ImageNet and provided by Keras, but you can
+    # substitute in your own networks just as easily)
+    global model
+    model = keras.models.load_model('./net4.h5')
+    model._make_predict_function()
+
+# Serve static files
+@app.route("/public/<path:path>")
+def public_files(path):
+    return send_from_directory('public', path)
+
+# Main page
+@app.route("/")
+def index():
+    return send_file('public/index.html')
+
+# Prediction endpoint
+@app.route("/predict", methods=['POST'])
+def predict():
+    classes = [
+        'flamingo',
+        'cake',
+        'campfire',
+        'angel',
+        'palm tree',
+        'remote control',
+        'rhinoceros',
+        'The Eiffel Tower',
+        'The Mona Lisa',
+        'wine bottle',
+        'hot air balloon',
+        'skateboard',
+        'map',
+        'underwear',
+        'roller coaster'
+    ]
+
+    image_data_url = request.json['image']
+    image_string = re.search(r'base64,(.*)', image_data_url).group(1)
+    image_bytes = io.BytesIO(base64.b64decode(image_string))
+    PIL_image = Image.open(image_bytes)
+    image_arr = np.array(PIL_image)[:, :, 0] / 255.0
+    image_arr = image_arr.flatten()
+    image_input = np.expand_dims(image_arr, axis=0)
+
+    predictions = model.predict(image_input)
+
+    ret = json.dumps({'conf': str(predictions.max() * 100)[:4], 'class': classes[np.argmax(predictions)]})
+    return ret
+
+if __name__ == "__main__":
+    print(("* Loading Keras model and Flask starting server..."
+        "please wait until server has fully started"))
+    load_model()
+    app.run(debug=True)
